28ad30cbbf82d2e9026fc5f81490a296d43bba1a
Jayson Herrera <jayson182005@gmail.com>
2023-05-11 15:19:00 +0800

    :wrench: Rebuild login, registration and dashboard to fix bugs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SanctumAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +17,3 @@ use App\Http\Controllers\SanctumAuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('register', [SanctumAuthController::class, 'register']);
-Route::post('login', [SanctumAuthController::class, 'login']);
-
-
