7f025acb270929c439e7fca4a36faec2b590f81e
Jayson Herrera <jayson182005@gmail.com>
2023-05-12 23:42:04 +0800

    :fire: fix dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Unit;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Customer;
+use App\Models\Supplier;
 use Illuminate\Database\Seeder;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +20,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+        ]);
+
+        Customer::factory(15)->create();
+        Supplier::factory(15)->create();
+
+        Category::factory(5)->create();
+        Unit::factory(5)->create();
+
+        for ($i=0; $i < 10; $i++) {
+            Product::factory()->create([
+                'product_code' => IdGenerator::generate([
+                    'table' => 'products',
+                    'field' => 'product_code',
+                    'length' => 4,
+                    'prefix' => 'PC'
+                ]),
+            ]);
+        }
+
     }
 }
--- resources/views/components/application-logo.blade.php
+++
@@ -1,3 +0,0 @@
-
-<img src="https://inevelle.sirv.com/assets/images/logo/Print-Studio/images/logo/logo.svg" alt="" style="width: 500px;" >
-
