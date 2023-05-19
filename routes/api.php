5db754a4b84f93425e7a389202f5d4781e8d8e90
unknown <marbella.tino@gmail.com>
2023-05-19 12:57:14 +0800

    first commit

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,6 @@ use App\Http\Controllers\Api\V1\CustomerController;
 use App\Http\Controllers\Api\V1\ProductController;
 use App\Http\Controllers\SanctumAuthController;
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::prefix('v1')->group(function() {
     Route::apiResource('/api-customers', CustomerController::class);
     Route::apiResource('/api-products', ProductController::class);
